2481f8e6a76749e82790f82b058d20488cfd8f93
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-06-22 12:41:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Dashboard } from "./page";
+import "./App.css";
+
+function App() {
+  return <Dashboard />;
+}
+
+export default App;
---
+++ src/page/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <main>
+      <h1 className="text-3xl font-medium"> Dashboard</h1>
+    </main>
+  );
+}
+
+export { Dashboard };
