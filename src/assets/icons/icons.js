124314b6e3e7f037f6fbfb93ae68ae09dd38e1f1
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-06-23 21:02:50 +0530

    Create icons.js

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,11 @@
+export {
+  FaAngleDoubleLeft,
+  FaAngleDoubleRight,
+  FaRegEdit,
+} from "react-icons/fa";
+export {
+  MdKeyboardArrowRight,
+  MdKeyboardArrowLeft,
+  MdOutlineDeleteOutline,
+} from "react-icons/md";
+export { CgClose } from "react-icons/cg";
